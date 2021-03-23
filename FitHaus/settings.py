073bc266a6f06a8e6aa39ba84b01f782ab6496aa
sergiii24 <sergividiellacurto>
2021-03-23 23:16:24 +0100

    Add postgresql database

--- FitHaus/settings.py
+++ FitHaus/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'FitHaus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'changeme',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
