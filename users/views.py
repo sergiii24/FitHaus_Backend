ba65315aa016ea62e8d32ba4e10c87a2dd87ed61
Joel <joel.crespo.i@estudiantat.upc.edu>
2021-04-18 17:52:07 +0200

    Updated views (useless stuff removed)

--- users/views.py
+++ users/views.py
@@ -13,34 +13,6 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-@api_view(['GET', 'POST'])
-def api_root(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            postea(serializer.get_attribute('email'))
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-def api_root(request, username):
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'DELETE':
-        user.delete()
-        return Response(status=status.HTTP_200_OK)
-
-
 @api_view(['GET'])
 def login(request):
     m = request.GET['email']
@@ -61,15 +33,12 @@ def postea(m):
     to = [m]
     subject = 'OMG Super Important Message'
     body = "Hey, what's up?\n\n- You"
-
     email_text = "Registre completat, enhorabona!"
-
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 587)
         server.login(gmail_user, gmail_password)
         server.sendmail(sent_from, to, email_text)
         server.close()
-
         print("Email sent!")
     except Exception as e:
         print("{}".format(e))
