32990e18580a772ceea67b025ff2fbdf4fa46b7e
sergi.vidiella <sergi.vidiella@upc.edu>
2021-05-30 20:40:57 +0200

    fix errors

--- app/fithaus/asgi.py
+++ app/fithaus/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FitHaus project.
+ASGI config for fithaus project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- app/fithaus/wsgi.py
+++ app/fithaus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FitHaus project.
+WSGI config for fithaus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
