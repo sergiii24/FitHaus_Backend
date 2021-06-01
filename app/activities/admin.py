3c3c8c45aa728ce32650cbe63b12e975649d756a
sergi.vidiella <sergi.vidiella@upc.edu>
2021-06-01 18:44:07 +0200

    fix errors

--- app/activities/admin.py
+++ app/activities/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from activities.models import Activity
 
-admin.register(Activity)
+admin.site.register(Activity)
--- app/exercises/admin.py
+++ app/exercises/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from exercises.models import Exercise
 
-admin.register(Exercise)
+admin.site.register(Exercise)
