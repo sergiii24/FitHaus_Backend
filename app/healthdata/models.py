327c8c87636fdf76b265f82d983fce3796ad5f51
Alejandro S <alejandro.santandreu@estudiantat.upc.edu>
2021-06-02 10:30:54 +0200

    health data worksssss

---
+++ app/healthdata/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from users.models import User
+from computed_property import ComputedFloatField
+import datetime
+
+
+class HealthData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    weight = ComputedFloatField(compute_from='calc_weight', default=1)
+    height = ComputedFloatField(compute_from='calc_height', default=1)
+    imc = ComputedFloatField(compute_from='calc_imc', default=1)
+    igc = ComputedFloatField(compute_from='calc_igc', default=1)
+    date = models.DateField(auto_now=True)
+
+    @property
+    def calc_weight(self):
+        weight = self.user.weight
+        return weight
+
+    @property
+    def calc_height(self):
+        height = self.user.height
+        return height
+
+    @property
+    def calc_imc(self):
+        imc = self.user.weight / ((self.user.height / 100) * (self.user.height / 100))
+        return imc
+
+    @property
+    def calc_igc(self):
+        edat = datetime.date.today().year - self.user.birthdate.year
+        sexe = ('M' == self.user.gender)
+        if edat < 16:
+            if sexe:
+                igc = 1.51 * self.imc - 0.7 * edat - 3.6 + 1.4
+            else:
+                igc = 1.51 * self.imc - 0.7 * edat + 1.4
+        else:
+            if sexe:
+                igc = 1.39 * self.imc + 0.16 * edat - 10.34 - 9
+            else:
+                igc = 1.39 * self.imc + 0.16 * edat - 9
+        return igc
+
+
+class HealthDataDTO(models.Model):
+    user_id = models.IntegerField(default=None)
+    weight = models.FloatField(default=1)
+    height = models.FloatField(default=1)
+    imc = models.FloatField(default=1)
+    igc = models.FloatField(default=1)
+    date = models.DateField()
---
+++ app/healthdata/views.py
@@ -0,0 +1,56 @@
+from rest_framework import viewsets, status
+from healthdata.models import HealthData, HealthDataDTO
+from healthdata.serializers import HealthDataSerializer, HealthDataDTOSerializer
+from rest_framework.response import Response
+from users.models import User
+from rest_framework.parsers import JSONParser
+import datetime
+
+
+class HealthDataViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = HealthData.objects.all()
+        id = request.query_params.get('userid')
+        if id is not None:
+            user = User.objects.get(id=id)
+            queryset = queryset.filter(user=user)
+        serializer = HealthDataSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        try:
+            data = JSONParser().parse(request)
+            pk = data.get('userid')
+            user = User.objects.get(id=pk)
+            if user is not None:
+                hd = HealthData.objects.create(user=user)
+                hd.save()
+                hdto = HealthDataDTO(id=hd.id,
+                                     user_id=user.id,
+                                     weight=user.weight,
+                                     height=user.height,
+                                     imc=hd.imc,
+                                     igc=hd.igc,
+                                     date=hd.date)
+                serialized = HealthDataDTOSerializer(hdto)
+                return Response(serialized.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        try:
+            hd = HealthData.objects.get(id=pk)
+            user = User.objects.get(id=hd.user.id)
+            hdto = HealthDataDTO(id=hd.id,
+                                 user_id=user.id,
+                                 weight=user.weight,
+                                 height=user.height,
+                                 imc=hd.imc,
+                                 igc=hd.igc,
+                                 date=hd.date)
+            serialized = HealthDataDTOSerializer(hdto)
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        except HealthData.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
