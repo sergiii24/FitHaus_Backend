437c98658714b9561f2d74a3895ccdc99c56b219
sergi.vidiella <sergi.vidiella@upc.edu>
2021-06-02 22:30:22 +0200

    Fix errors settings

--- app/fithaus/settings.py
+++ app/fithaus/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'changeme',
-        'HOST': 'localhost',  # localhost or postgres
+        'HOST': 'postgres',  # localhost or postgres
         'PORT': '5432',
     }
 }
